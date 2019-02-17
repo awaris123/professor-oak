1b128482978dfb127d90b361cac7333f9a6878ec
Aakef Waris <awaris@hawk.iit.edu>
2019-02-17 01:33:26 -0600

    created route for post method

--- app.js
+++ app.js
@@ -28,20 +28,31 @@ app.get('/', function (req, res) {
 
 
 app.get('/generate', function(req, res){
-  // var pref = {
-  //   "HP":req.body.HP,
-  //   "Atk": req.body.Attack,
-  //   "Def": req.body.Defense,
-  //   "SpAtk": req.body.SpAttack,
-  //   "SpDef": req.body.SpDefense,
-  //   "Spd": req.body.Speed
-  // }
-  // console.log(pref)
+
+
+
   res.render('gen');
-})
+});
+
+app.post('/show', function(req, res){
+  const spawn = require("child_process").spawn;
+  const pythonProcess = spawn('python',["pokemonpy.py"])
+  pythonProcess.stdout.on('data', function(data) {
+        console.log(data.toString());
+      } )
+  var pref = {
+    "HP":req.body.HP,
+    "Atk": req.body.Attack,
+    "Def": req.body.Defense,
+    "SpAtk": req.body.SpAttack,
+    "SpDef": req.body.SpDefense,
+    "Spd": req.body.Speed
+  }
+  console.log(pref)
+});
 
 
 
 app.listen(8080, function () {
-  console.log('App listening on LocalHost:3000!');
+  console.log('App listening on LocalHost:8080!');
 });
