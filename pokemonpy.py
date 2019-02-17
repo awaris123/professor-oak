af005ca36ea5c48109323745cf4b67d836041ebd
faipat1 <faisalpatel64@gmail.com>
2019-02-17 01:41:05 -0600

    updated py script

--- pokemonpy.py
+++ pokemonpy.py
@@ -11,19 +11,6 @@ from operator import itemgetter
 
 pokedex = []
 sortedList = []
-
-def hello():
-    print("Hello")
-    
-def printFile():   
-    with open('Pokemon_all.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count < 100:
-                print(f'{row[0]} | {row[1]} | {row[2]} | {row[3]} | {row[4]} | {row[5]} | {row[6]} | {row[7]} | {row[8]} | {row[9]} | {row[10]} | {row[12]} | {row[13]}')
-                line_count += 1      
-        print(f'Processed {line_count} lines.')
     
 def createPokedex():
     with open('Pokemon_all.csv') as csv_file:
