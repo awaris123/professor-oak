3549b744a48137f6a0ca3eda6312f19addf2fd0e
faipat1 <faisalpatel64@gmail.com>
2019-02-17 01:32:16 -0600

    updated py script

--- pokemonpy.py
+++ pokemonpy.py
@@ -33,7 +33,7 @@ def createPokedex():
             if line_count <= 0:
                 line_count += 1
             else:
-                pokedex.append({'id':row[0], 'name':row[1], 'type1':row[2], 'type2':row[3], 'total':row[4], 'hp':row[5], 'attack':row[6], 'defense':row[7], 'spatk':row[8], 'spdef':row[9], 'speed':row[10], 'generation':row[11], 'legendary':row[12], 'mega':row[13]})
+                pokedex.append({'id':row[0], 'name':row[1], 'type1':row[2], 'type2':row[3], 'total':row[4], 'hp':row[5], 'attack':row[6], 'defense':row[7], 'spatk':row[8], 'spdef':row[9], 'speed':row[10], 'generation':row[11], 'legendary':row[12], 'mega':row[13], 'coeff':0})
                 line_count += 1
     return pokedex
     print(f'Processed {line_count} lines.')
@@ -46,24 +46,39 @@ def pokeSort():
     else:
         sortedList = sorted(pokedex, key=lambda item: int(item[str(sorter)]))
         return sortedList
-    
-createPokedex()
-for item in pokeSort():
-    print(str(item) + '\n')
 
-
-'''      
-def attackSort():
+def coeffSort():
     sortedList = pokedex
-    sortedList = sorted(pokedex, key=lambda item: int(item['attack']))
-    return sortedList
-'''
+    sortedList = sorted(pokedex, key=lambda item: int(item[str('coeff')]))
+    legendary = input("Do you want to filter out legendaries?")
+    mega = input("Do you want to filter out megas?")
+    tempList = []
+    for p in sortedList:
+        if legendary == 'no' and mega == 'no':
+            tempList.append(p)
+        elif legendary == 'yes' and mega == 'yes':
+            if p['legendary'] == 'FALSE' and p['mega'] == 'FALSE':
+                tempList.append(p)
+        elif legendary == 'yes' and mega == 'no':
+            if p['legendary'] == 'FALSE':
+                tempList.append(p)
+        elif legendary == 'no' and mega == 'yes':
+            if p['mega'] == 'FALSE':
+                tempList.append(p)
+                
+    for p in tempList:
+        print(str(p) +'\n')
+    return tempList
 
-'''
-for item in attackSort():
-    print(str(item) + '\n -------------------------------------------------------------------------------')
-'''
+def priorityVal():
+    print("Enter priority values for hp,att,def,spatk,spdef,speed: (seperated by spaces")
+    prio = [int(x) for x in input().split()]
+    for p in pokedex:
+        p['coeff'] = int(p['hp'])/prio[0] + int(p['attack'])/prio[1] + int(p['defense'])/prio[2] + int(p['spatk'])/prio[3] + int(p['spdef'])/prio[4] + int(p['speed'])/prio[5]
+    coeffSort()
 
 
+createPokedex()    
+priorityVal()
 
     
