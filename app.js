18ecc6c37ed36ad5f6417d17f4974ca88b13e7e7
Aakef Waris <awaris@hawk.iit.edu>
2019-02-17 03:50:24 -0600

    added JSON for preferences

--- app.js
+++ app.js
@@ -1,58 +1,62 @@
 
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var body = require('body-parser');
 var path = require('path');
 
-
 // Body Parser MiddleWare
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-
-
+app.use(body.json());
+app.use(body.urlencoded({extended:false}));
 
 // View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'static')))
 
-
-
 // Main Route
 app.get('/', function (req, res) {
+
   res.render('index');
+
 });
 
 
 app.get('/generate', function(req, res){
 
-
-
   res.render('gen');
+
 });
 
 app.post('/show', function(req, res){
-  const spawn = require("child_process").spawn;
-  const pythonProcess = spawn('python',["pokemonpy.py"])
-  pythonProcess.stdout.on('data', function(data) {
-        console.log(data.toString());
-      } )
+
   var pref = {
     "HP":req.body.HP,
     "Atk": req.body.Attack,
     "Def": req.body.Defense,
     "SpAtk": req.body.SpAttack,
     "SpDef": req.body.SpDefense,
-    "Spd": req.body.Speed
+    "Spd": req.body.Speed,
   }
+
+  const spawn = require("child_process").spawn;
+  const pythonProcess = spawn('python',["pokemonpy.py", pref])
+  pythonProcess.stdout.on('data', function(data) {
+        console.log(data.toString());
+      })
+
+
   console.log(pref)
+
+  res.render("show")
+
 });
 
 
 
 app.listen(8080, function () {
+
   console.log('App listening on LocalHost:8080!');
+
 });
